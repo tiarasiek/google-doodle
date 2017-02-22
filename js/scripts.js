f3b720c2e8947bfad952d9b08c68d5b07960c391
tiarasiek <tsiek@vcd-ch204-22.eastern.ewu.edu>
2017-02-22 13:45:05 -0800

    Randomize image
    *Added images
    *Random homepage image
    *JS to randomize

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,12 @@
-// YOUR SCRIPTS GO HERE
+// Store 3 images
+var img0 = '../img/200.jpg';
+var img1 = '../img/300.jpg';
+var img2 = '../img/400.jpg';
+
+// Get random number
+var max = 3;
+var numRand = Math.floor(Math.random() * max);
+console.log(numRand);
+
+// Display random image
+$('img').attr('src', eval('img' + numRand));
