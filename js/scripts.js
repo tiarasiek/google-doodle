103f7a18dafde1cffe377ec418ff9cba0d6dee55
tiarasiek <User@Users-MacBook-Pro.local>
2017-03-03 15:59:59 -0800

    Drawsvg animation, added svg
    *Added js for drawsvg
    *Added G svg
    *Deleted example images/svgs
    *Resized G svg

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,14 @@ console.log(numRand)
 
 // Change class on body element
 $('#stage').addClass('bg' + numRand);
+
+// Draw SVG
+// Store SVG points
+var google = $('#spiderweb').drawsvg({
+  duration : 2500,
+  reverse: true,
+  stagger: 1000
+});
+
+// Animate SVG points
+google.drawsvg('animate');
